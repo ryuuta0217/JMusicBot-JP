a5fc10e6c75428e3f88464776ccb88f73d53ba38
kosugikun <kosukisaki@yahoo.co.jp>
2019-07-05 07:28:48 +0900

    少し翻訳

--- src/main/java/com/jagrosh/jmusicbot/utils/FormatUtil.java
+++ src/main/java/com/jagrosh/jmusicbot/utils/FormatUtil.java
@@ -62,31 +62,31 @@ public class FormatUtil {
     
     public static String listOfTChannels(List<TextChannel> list, String query)
     {
-        String out = " Multiple text channels found matching \""+query+"\":";
+        String out = " 複数のテキストチャンネルで\""+query+"\"が一致しました。:";
         for(int i=0; i<6 && i<list.size(); i++)
             out+="\n - "+list.get(i).getName()+" (<#"+list.get(i).getId()+">)";
         if(list.size()>6)
-            out+="\n**And "+(list.size()-6)+" more...**";
+            out+="\n**と "+(list.size()-6)+" など...**";
         return out;
     }
     
     public static String listOfVChannels(List<VoiceChannel> list, String query)
     {
-        String out = " Multiple voice channels found matching \""+query+"\":";
+        String out = " 複数のボイスチャンネルで\""+query+"\"が一致しました。:";
         for(int i=0; i<6 && i<list.size(); i++)
             out+="\n - "+list.get(i).getName()+" (ID:"+list.get(i).getId()+")";
         if(list.size()>6)
-            out+="\n**And "+(list.size()-6)+" more...**";
+            out+="\n**と "+(list.size()-6)+" など...**";
         return out;
     }
     
     public static String listOfRoles(List<Role> list, String query)
     {
-        String out = " Multiple text channels found matching \""+query+"\":";
+        String out = " 複数のテキストチャンネルで \""+query+"\"が一致しました。:";
         for(int i=0; i<6 && i<list.size(); i++)
             out+="\n - "+list.get(i).getName()+" (ID:"+list.get(i).getId()+")";
         if(list.size()>6)
-            out+="\n**And "+(list.size()-6)+" more...**";
+            out+="\n**と "+(list.size()-6)+" など...**";
         return out;
     }
     
