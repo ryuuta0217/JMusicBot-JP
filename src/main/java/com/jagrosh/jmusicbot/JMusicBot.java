92d24b20ab7309e0e64baf0ea938afd40653e427
kosugikun <kosukisaki@yahoo.co.jp>
2019-07-05 19:36:51 +0900

    修正

--- src/main/java/com/jagrosh/jmusicbot/JMusicBot.java
+++ src/main/java/com/jagrosh/jmusicbot/JMusicBot.java
@@ -161,8 +161,10 @@ public class JMusicBot
             catch(Exception e) 
             {
                 log.error("GUIを起動できませんでした。あなたがいる場合 "
-                         + "サーバー上、または表示できない場所で実行されている"
-                         + "ウィンドウ、-Dnogui=trueフラグを使用してnoguiモードで実行してください。");
+
+                        + "サーバー上、または表示できない場所で実行されている"
+
+                        + "ウィンドウ、-Dnogui=trueフラグを使用してnoguiモードで実行してください。");
             }
         }
         
@@ -184,15 +186,10 @@ public class JMusicBot
         catch (LoginException ex)
         {
             prompt.alert(Prompt.Level.ERROR, "JMusicBot", ex + "\nあなたがいることを確認してください"
-<<<<<<< HEAD
 
                     + "正しいconfig.txtファイルを編集し、あなたが使ったことがある"
 
                     + "正しいトークン('secret'ではありません!)\n設定場所："+config.getConfigLocation());
-=======
-                     + "正しいconfig.txtファイルを編集し、あなたが使ったことがある"
-                     + "正しいトークン('secret'ではありません!)\n設定場所："+config.getConfigLocation());
->>>>>>> eddde061dae873679cfeae361f2169b4739eba48
             System.exit(1);
         }
         catch(IllegalArgumentException ex)
