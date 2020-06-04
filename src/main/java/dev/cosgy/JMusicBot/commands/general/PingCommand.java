dd0bb544d343ebf31c0f96e2b20466cb8891f5be
ryuuta0217 <ryuuta.iwakura@gmail.com>
2020-06-04 20:57:23 +0900

    About/Pingコマンドクラスのパッケージ移動

--- src/main/java/dev/cosgy/JMusicBot/commands/general/PingCommand.java
+++ src/main/java/dev/cosgy/JMusicBot/commands/general/PingCommand.java
@@ -13,7 +13,7 @@
  *   See the License for the specific language governing permissions and
  *   limitations under the License.
  */
-package dev.cosgy.JMusicBot.commands.examples;
+package dev.cosgy.JMusicBot.commands.general;
 
 import com.jagrosh.jdautilities.command.Command;
 import com.jagrosh.jdautilities.command.CommandEvent;
